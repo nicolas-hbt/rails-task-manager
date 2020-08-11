74124f1830c63bb6719bcb6fcc9102fb27085a3d
Nicolas Hubert <nicolas.hubert@eleves.ec-nantes.fr>
2020-08-11 12:00:49 +0200

    Add exo 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,52 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.update(task_params)
+
+    # no need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def show
+    # @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
